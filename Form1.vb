bb9758fd53dbcb588c78af8e20ac86ff871de23f
Blandd <atspencer2008@gmail.com>
2024-03-26 08:31:29 -0400

    cleaned up

--- Form1.vb
+++ Form1.vb
@@ -49,9 +49,7 @@ Public Class Form1
         'Dim str As String = reader.ReadToEnd
         'words = JsonConvert.DeserializeObject(Of List(Of Questions))(str)
         'reader.Close()
-        'Tony asked a good question 
 
-        'Tony's a bum
     End Sub
 End Class
 Class Questions
